56f47a6580709af8ec122a1d23031db0568c3740
eduairet <hola@eduairet.com>
2022-02-23 19:29:03 -0600

    README añadido

---
+++ README.md
@@ -0,0 +1,7 @@
+# 36 Days Of Typecooker 2022
+
+Este documento genera las imágenes de las recetas diarias para el **36 Days of Typecooker,** una actividad que da parámetros de dibujo generados por la herramienta (Typecooker, creada por Erik van Blokland,)[https://typecooker.com/] para que cualquier persona pueda unirse al **36 Days of Type** sin ese miedo que genera la página en blanco.
+
+Visita la (página oficial de 36 Days of Type)[https://www.36daysoftype.com] para conocer la dinámica u calendaroi y sigue (nuestro instagram)[https://www.instagram.com/letrasticafest/] para no perderte ninguna receta también (únete a nuestro Discord)[https://discord.gg/Ez29XBYbGn] para compartir tu resultado en comunidad.
+
+**¡A cocinar letras!**
---
+++ generadorDe36DaysOfType2022.py
@@ -0,0 +1,31 @@
+'''
+Este documento genera las imágenes de promoción de las recetas diarias para el 36 Days of Typecooker 2022
+'''
+
+# Módulos requeridos para este script
+import os
+import csv
+from collections import namedtuple
+
+# Variables para funcionamiento
+# ·····························
+
+# Rutas relevantes
+datosCarpeta = os.path.join( os.path.split(__file__)[0], 'datos' )
+datos = os.path.join(datosCarpeta, 'datos36DaysOfType2022.csv')
+# Named tuple para generar la estructura de cada archivo de salida
+FormatoDeSalida = namedtuple('FormatoDeSalida',['x','y'])
+# Diccionario con los archivos de salida
+formatosDeSalida = {
+    'instaFeed': FormatoDeSalida(x=1080, y=1080),
+    'instaStory': FormatoDeSalida(x=1080, y=1920),
+}
+
+# ·····························
+
+# Área de diseño
+# ·····························
+
+for formato in formatosDeSalida.values():
+    ancho, alto = formato.x, formato.y
+    newPage(ancho, alto)
